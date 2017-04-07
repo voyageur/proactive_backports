6608ca7b9af795177cf627f304fab772c411940c
Ihar Hrachyshka <ihrachys@redhat.com>
2017-04-06 23:47:09 -0700

    Make os-clone.sh use zuul-cloner
    ...and use local cache for upstream git.

--- os-clone.sh
+++ os-clone.sh
@@ -3,8 +3,15 @@
 set -e
 set -x
 
-arg=$(echo $1 | tr "/" "\n")
+cloner='zuul-cloner https://git.openstack.org'
 
+if [ -z "${ZUUL_CACHE_DIR}" ]; then
+    echo "Please define ZUUL_CACHE_DIR." 1>&2
+    exit 1
+fi
+mkdir -p "${ZUUL_CACHE_DIR}"
+
+arg=$(echo $1 | tr "/" "\n")
 if [ ${#arg[@]} -eq 2 ]; then
     namespace=${arg[0]}
     project=${arg[1]}
@@ -12,17 +19,14 @@ else
     namespace=openstack
     project=${arg[0]}
 fi
-
-echo $namespace
-echo $project
-
 proj=${namespace}/${project}
-clonedir=~/git/${proj}
-if [ -d ${clonedir} ]; then
-    exit 0
-fi
 
-mkdir -p ~/git/${namespace}
-git clone https://git.openstack.org/${proj} ${clonedir}
+# first prepopulate cache
+pushd ${ZUUL_CACHE_DIR}
+${cloner} ${proj}
+popd
+
+# now check out in current dir (from cache)
+${cloner} ${proj}
 
 exit 0
